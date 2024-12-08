b53060f61d9c3f7f4ecc5b5cd0b36b1f077f1768
fktk <windwild12@gmail.com>
2024-12-08 13:43:08 +0900

    nvim-cmpを追加

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,4 +1,3 @@
-
 return {
     {
         'folke/tokyonight.nvim',
@@ -99,6 +98,25 @@ return {
             pcall(require('telescope').load_extension, 'ui-select')
         end,
     },
+    { -- Highlight, edit, and navigate code
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
+        main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+        -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+        opts = {
+            ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+            -- Autoinstall languages that are not installed
+            auto_install = true,
+            highlight = {
+                enable = true,
+                -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+                --  If you are experiencing weird indenting issues, add the language to
+                --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+                additional_vim_regex_highlighting = { 'ruby' },
+            },
+            indent = { enable = true, disable = { 'ruby' } },
+        },
+    },
     -- LSP Plugins
     {
         -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
@@ -114,4 +132,59 @@ return {
     },
     { 'Bilal2453/luvit-meta', lazy = true },
 
+    { -- Autocompletion
+        'hrsh7th/nvim-cmp',
+        event = 'InsertEnter',
+        dependencies = {
+            -- Adds other completion capabilities.
+            --  nvim-cmp does not ship with all sources by default. They are split
+            --  into multiple repos for maintenance purposes.
+            --
+            -- 'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-path',
+        },
+        config = function()
+            -- See `:help cmp`
+            local cmp = require 'cmp'
+
+            cmp.setup {
+                completion = { completeopt = 'menu,menuone,noinsert' },
+
+                -- For an understanding of why these mappings were
+                -- chosen, you will need to read `:help ins-completion`
+                --
+                -- No, but seriously. Please read `:help ins-completion`, it is really good!
+                mapping = cmp.mapping.preset.insert {
+                    -- Select the [n]ext item
+                    ['<C-n>'] = cmp.mapping.select_next_item(),
+                    -- Select the [p]revious item
+                    ['<C-p>'] = cmp.mapping.select_prev_item(),
+
+                    -- Scroll the documentation window [b]ack / [f]orward
+                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+
+                    -- Accept ([y]es) the completion.
+                    --  This will auto-import if your LSP supports it.
+                    --  This will expand snippets if the LSP sent a snippet.
+                    ['<C-y>'] = cmp.mapping.confirm { select = true },
+
+                    -- If you prefer more traditional completion keymaps,
+                    -- you can uncomment the following lines
+                    --['<CR>'] = cmp.mapping.confirm { select = true },
+                    --['<Tab>'] = cmp.mapping.select_next_item(),
+                    --['<S-Tab>'] = cmp.mapping.select_prev_item(),
+                },
+                sources = {
+                    {
+                        name = 'lazydev',
+                        -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
+                        group_index = 0,
+                    },
+                    -- { name = 'nvim_lsp' },
+                    { name = 'path' },
+                },
+            }
+        end,
+    },
 }
