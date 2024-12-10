dff0d8197f15c8e950cfd1b6a675cf96eb0b2ba0
fktk <windwild12@gmail.com>
2024-12-10 22:47:02 +0900

    ruffを入れた

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -10,7 +10,9 @@ return {
   {
     'windwp/nvim-autopairs',
     event = 'InsertEnter',
-    config = true
+    opts = {
+      map_c_w = true,
+    }
   },
   {
     'shellRaining/hlchunk.nvim',
@@ -223,6 +225,12 @@ return {
               end,
             })
           end
+          if client == nil then
+            return
+          end
+          if client.name == 'ruff' then
+            client.server_capabilities.hoverProvider = false
+          end
         end,
       })
 
@@ -239,23 +247,28 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
-
+        pyright = {
+          settings = {
+            python = {
+              pythonPath = '.venv/bin/python',
+              analysis = {
+                ignore = { '*' },
+              },
+            },
+            pyright = {
+              disableOrganizeImports = true,
+            },
+          }
+        },
+        ruff = {
+          -- settings = {
+          --   init_options = {
+          --     settings = {
+          --     },
+          --   },
+          -- }
+        },
         lua_ls = {
-          -- cmd = {...},
-          -- filetypes = { ...},
-          -- capabilities = {},
           settings = {
             Lua = {
               completion = {
