3d22cd0b3a39704b5f6cbf9f16ad7fe8a5b69ba2
fktk <windwild12@gmail.com>
2024-12-15 20:36:22 +0900

    AIのプラグインを分離

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -188,24 +188,4 @@ return {
 
     end,
   },
-  {
-    "jackMort/ChatGPT.nvim",
-    event = "VeryLazy",
-    config = function()
-      vim.fn.setenv('OPENAI_API_KEY', 'dummy')
-      vim.fn.setenv('OPENAI_API_HOST', 'http://localhost:8080')
-      -- vim.fn.setenv('OPENAI_API_HOST', '')
-      require("chatgpt").setup({
-        openai_params = {
-          max_tokens = 1000,
-        }
-      })
-    end,
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "folke/trouble.nvim", -- optional
-      "nvim-telescope/telescope.nvim"
-    }
-  },
 }
