ed714337e6aae4a2ec88ed0ec81d40dfb533ae48
fktk <windwild12@gmail.com>
2024-12-15 23:23:36 +0900

    avante.nvimを追加して無効にしている

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -19,4 +19,68 @@ return {
       "nvim-telescope/telescope.nvim"
     }
   },
+  -- {
+  --   "yetone/avante.nvim",
+  --   event = "VeryLazy",
+  --   lazy = false,
+  --   version = false, -- set this if you want to always pull the latest change
+  --   opts = {
+  --     -- add any opts here
+  --     provider = 'openai',
+  --     auto_suggestions_provider = 'openai',
+  --     openai = {
+  --       endpoint = 'http://localhost:8080/v1',
+  --       allow_insecure = true,
+  --     },
+  --     behaviour = {
+  --       auto_suggestions = false,
+  --       auto_apply_diff_after_generation = true,
+  --     },
+  --     windows = {
+  --       edit = {
+  --         start_insert = false,
+  --       },
+  --       ask = {
+  --         start_insert = false,
+  --       }
+  --     },
+  --   },
+  --   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  --   build = "make",
+  --   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  --   dependencies = {
+  --     "stevearc/dressing.nvim",
+  --     "nvim-lua/plenary.nvim",
+  --     "MunifTanjim/nui.nvim",
+  --     --- The below dependencies are optional,
+  --     "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+  --     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+  --     -- "zbirenbaum/copilot.lua", -- for providers='copilot'
+  --     {
+  --       -- support for image pasting
+  --       "HakonHarnes/img-clip.nvim",
+  --       event = "VeryLazy",
+  --       opts = {
+  --         -- recommended settings
+  --         default = {
+  --           embed_image_as_base64 = false,
+  --           prompt_for_file_name = false,
+  --           drag_and_drop = {
+  --             insert_mode = true,
+  --           },
+  --           -- required for Windows users
+  --           use_absolute_path = true,
+  --         },
+  --       },
+  --     },
+  --     {
+  --       -- Make sure to set this up properly if you have lazy=true
+  --       'MeanderingProgrammer/render-markdown.nvim',
+  --       opts = {
+  --         file_types = { "markdown", "Avante" },
+  --       },
+  --       ft = { "markdown", "Avante" },
+  --     },
+  --   },
+  -- }
 }
