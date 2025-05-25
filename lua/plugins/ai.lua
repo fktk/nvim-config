b38d044f5ac44c1b6de0b5c9f8b0aa05ae622a16
fktk <windwild12@gmail.com>
2025-05-25 16:43:19 +0900

    CopilotChatの基本的な設定を完了した

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -7,23 +7,26 @@ return {
       require('mcphub').setup()
     end,
   },
-  -- {
-  --   'CopilotC-Nvim/CopilotChat.nvim',
-  --   dependencies = {
-  --     { 'zbirenbaum/copilot.lua' },
-  --     { 'nvim-lua/plenary.nvim', branch = 'master' },
-  --   },
-  --   -- build = 'make tiktoken',
-  --   opts = {
-  --     mappings = {
-  --       submit_prompt = {
-  --         normal = '<C-l>',
-  --         insert = '<C-l>',
-  --       },
-  --     },
-  --   },
-  --   config = function(_, opts)
-  --     require('CopilotChat').setup(opts)
-  --   end,
-  -- },
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    dependencies = {
+      { 'zbirenbaum/copilot.lua' },
+      { 'nvim-lua/plenary.nvim', branch = 'master' },
+      { 'ravitemer/mcphub.nvim'},
+    },
+    -- build = 'make tiktoken',
+    opts = {
+      mappings = {
+        submit_prompt = {
+          -- normal = '<C-l>',
+          -- insert = '<C-l>',
+        },
+      },
+    },
+    config = function(_, opts)
+      local chat = require("CopilotChat")
+      chat.setup()
+
+    end,
+  },
 }
--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -208,7 +208,7 @@ return {
   },
   { -- Autocompletion
     'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
+    -- event = 'InsertEnter',
     dependencies = {
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
