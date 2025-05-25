e7f2bac2ba316a921493f9cf6e40fccd6f2e33ea
fktk <windwild12@gmail.com>
2025-05-25 15:34:47 +0900

    CopilotChatを試す

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -7,118 +7,23 @@ return {
       require('mcphub').setup()
     end,
   },
-  {
-    "olimorris/codecompanion.nvim",
-    init = function()
-      require('plugins.codecompanion.fidget-spinner'):init()
-    end,
-    opts = {
-      opts = { language = 'Japanese'},
-      display = {
-        chat = {
-          window = {
-            position = 'right',
-          },
-        },
-      },
-      extensions = {
-        mcphub = {
-          callback = 'mcphub.extensions.codecompanion',
-          opts = {
-            make_vars = true,
-            make_slash_commands = true,
-            show_result_in_chat = true,
-          },
-        },
-        history = {
-          enabled = true,
-        }
-      },
-      strategies = {
-        chat = {
-          adapter = 'copilot',
-          keymaps = {
-            send = {
-              modes = {
-                i = { "<C-l>" },
-                n = { "<C-l>" },
-              },
-            },
-          },
-        },
-        inline = {
-          adapter = 'copilot',
-          keymaps = {
-            send = {
-              modes = {
-                i = { "<C-l>" },
-                n = { "<C-l>" },
-              },
-            },
-          },
-        },
-        cmd = {
-          adapter = 'copilot'
-        },
-      },
-      adapters = {
-        copilot = function()
-          return require('codecompanion.adapters').extend('copilot', {
-            schema = {
-              model = {
-                -- default = "gpt-4.1",
-                default = "claude-3.5-sonnet",
-              },
-            },
-          })
-        end,
-      }
-    },
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter',
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      'nvim-telescope/telescope.nvim',
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua", -- for providers='copilot'
-      'j-hui/fidget.nvim',
-      {
-        'ravitemer/mcphub.nvim',
-        dependencies = { 'nvim-lua/plenary.nvim', },
-      },
-      {
-        'ravitemer/codecompanion-history.nvim',
-      },
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        cmd = "PasteImage",
-        event = "VeryLazy",
-        opts = {
-          filetype = {
-            codecompanion = {
-              prompt_for_name = false,
-              template = "[Image]($FILE_PATH)",
-              use_absolute_path = true,
-            }
-          }
-        },
-      },
-      -- {
-      --   'echasnovski/mini.diff',
-      --   config = function()
-      --     local diff = require('mini.diff')
-      --     diff.setup({
-      --       source = diff.gen_source.none(),
-      --     })
-      --   end,
-      -- },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        'MeanderingProgrammer/render-markdown.nvim',
-        ft = { "markdown", "codecompanion" },
-      },
-    },
-  },
+  -- {
+  --   'CopilotC-Nvim/CopilotChat.nvim',
+  --   dependencies = {
+  --     { 'zbirenbaum/copilot.lua' },
+  --     { 'nvim-lua/plenary.nvim', branch = 'master' },
+  --   },
+  --   -- build = 'make tiktoken',
+  --   opts = {
+  --     mappings = {
+  --       submit_prompt = {
+  --         normal = '<C-l>',
+  --         insert = '<C-l>',
+  --       },
+  --     },
+  --   },
+  --   config = function(_, opts)
+  --     require('CopilotChat').setup(opts)
+  --   end,
+  -- },
 }
