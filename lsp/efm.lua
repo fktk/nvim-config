3a279d7bcd5ce14e2c75e158b7d38d012d2e9523
fktk <windwild12@gmail.com>
2025-07-06 13:10:03 +0900

    efm-language-serverを追加

---
+++ lsp/efm.lua
@@ -0,0 +1,13 @@
+return {
+  init_options = {
+    documentFormatting = true,
+    dokumentRangeFormatting = true,
+  },
+  settings = {
+    rootMarkers = {'.git/'},
+    languages = {
+      textlint = { },
+    },
+  },
+  filetypes = { 'markdown' },
+}
